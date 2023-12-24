2b14102b7a11fedba4563768a68eb7b2d38dda75
root <root@HARIO404>
2023-12-24 22:00:02 +0800

    Update lagi

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 📍Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image (karsajob) 
+docker build -t ghcr.io/hario86/karsajobs:latest .
+
+# 📍Login ke Github Packages
+echo $CR_PAT | docker login ghcr.io -u hario86 --password-stdin
+
+# Mengunggah image ke Github Packages
+docker push ghcr.io/hario86/karsajobs:latest 
